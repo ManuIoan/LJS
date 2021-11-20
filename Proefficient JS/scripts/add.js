0672e5a644e89041cf1dfa008fea4f78ad171faa
Manu Ioan <ioanmanu@yahoo.com>
2021-11-20 10:31:54 +0200

    sdd

---
+++ Proefficient JS/scripts/add.js
@@ -0,0 +1,12 @@
+const form  = document.getElementById('add');
+
+form.addEventListener('submit', (event) => {
+    
+    event.preventDefault();
+    const name = form.elements['name'].value
+    const val = form.elements['val'].value
+    const text = form.elements['text'].value
+        
+
+    
+});
