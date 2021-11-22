06c40e94ad8e6246714b0a6506dfd9737fe3f762
Manu Ioan <ioanmanu@yahoo.com>
2021-11-22 11:32:15 +0200

    Canvas 1.0.2

---
+++ Proefficient JS/Canvas/incercare/script.js
@@ -0,0 +1,73 @@
+const canvas = document.getElementById("minecan")
+const c = canvas.getContext("2d")
+
+let tx = window.innerWidth;
+let ty = window.innerHeight;
+canvas.width = tx;
+canvas.height = ty;
+
+const circle={
+    x:500,
+    y:200,
+    size:30,
+    dx: 10,
+    dy:10,
+}
+
+const moveRight = ()=>{
+    circle.x += circle.dx
+}
+const moveLeft = ()=>{
+    circle.x -= circle.dx
+}
+const moveUp = ()=>{
+    circle.y -= circle.dy
+}
+const moveDown = ()=>{
+    circle.y -= circle.dy
+}
+
+
+function draw(){
+c.beginPath();
+c.arc(circle.x, circle.y, circle.size, 0, 2 * Math.PI);
+
+c.fillStyle = 'red';
+
+c.fill();
+}
+
+
+ function update()
+ {
+     c.clearRect(0,0,canvas.width, canvas.height)
+     draw()
+     
+
+     requestAnimationFrame(update)
+
+ }
+
+document.addEventListener('keydown',(e)=>{
+    if(e.key === 'ArrowRight' ||e.key === 'Right')
+        moveRight()
+        else 
+        if(e.key=== 'ArrowLeft' ||e.key=== 'Left')
+        moveLeft()
+        else 
+        if(e.key=== 'ArrowUp' ||e.key=== 'Up')
+        moveUp()
+        else 
+        if(e.key=== 'ArrowDown' ||e.key=== 'Down')
+        moveDown()
+
+
+})
+
+
+
+document.addEventListener('keyup',()=>{
+    
+})
+
+update()
