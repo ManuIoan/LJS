ccd0ec2a33ff29e550f9119788a916e01cc5274e
Manu Ioan <ioanmanu@yahoo.com>
2021-11-23 11:10:51 +0200

    10pin10h

---
+++ Proefficient JS/10 js proj in 10hours/countdown/script.js
@@ -0,0 +1,60 @@
+let then = new Date(2022, 2, 9, 0, 0)
+an = document.querySelector(".an")
+luna = document.querySelector(".luna")
+zile = document.querySelector(".zile")
+ore = document.querySelector(".ore")
+min = document.querySelector(".min")
+sec = document.querySelector(".sec")
+
+
+
+const day_m =()=>{
+     // instantiate a date object
+ var dt = new Date();
+
+ // dt.getMonth() will return a month between 0 - 11
+ // we add one to get to the last day of the month 
+ // so that when getDate() is called it will return the last day of the month
+  var month = dt.getMonth() + 1;
+  var year = dt.getFullYear();
+ 
+ // this line does the magic (in collab with the lines above)
+  var daysInMonth = new Date(year, month, 0).getDate();
+  return daysInMonth
+}
+
+const getTime= ()=>{
+let now = new Date()
+
+let y = then.getFullYear() - now.getFullYear()-1
+
+let mon_now =now.getMonth()
+let mon_then =then.getMonth()
+let m
+
+if(mon_now>mon_then)
+{
+    m= 12-mon_now+mon_then
+}
+else
+   m= mon_then- mon_now 
+
+let z = day_m()- now.getDate()
+ 
+let h = 23-now.getHours()
+let min2 = 59-now.getMinutes()
+let sec2 = 59-now.getSeconds()
+
+
+
+console.log(h)
+an.innerHTML= y
+luna.innerHTML= m
+zile.innerHTML= z
+ore.innerHTML= h
+min.innerHTML= min2
+sec.innerHTML= sec2
+
+
+}
+ setInterval(getTime, 1000)
---
+++ Proefficient JS/10 js proj in 10hours/quiz-app/script.js
@@ -0,0 +1,10 @@
+const cioc = document.querySelector("#c")
+
+
+
+
+cioc.addEventListener("click",(event)=>{
+    event.preventDefault()
+    console.log(454)
+
+})
